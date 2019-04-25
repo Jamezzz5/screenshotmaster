391ee9e57c8548e5f7431b047e417438d300cc81
Jamezzz5 <imirzian@umich.edu>
2019-04-25 14:20:58 -0400

    update image to binary to check for file

--- ssm/utils.py
+++ ssm/utils.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 config_path = 'config/'
 
@@ -9,6 +10,10 @@ def dir_check(directory):
 
 
 def image_to_binary(file_name):
-    with open(file_name, 'rb') as image_file:
-        image_data = image_file.read()
+    if os.path.isfile(file_name):
+        with open(file_name, 'rb') as image_file:
+            image_data = image_file.read()
+    else:
+        logging.warning('{} does not exist returning None'.format(file_name))
+        image_data = None
     return image_data
