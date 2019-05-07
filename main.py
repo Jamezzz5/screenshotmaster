c66819b250454f51d086955498ecff5ae8e60126
Jamezzz5 <imirzian@umich.edu>
2019-05-07 11:43:09 -0400

    added arguments and additional requirements

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import logging
+import argparse
 import ssm.site as sit
+import ssm.export as exp
 
 formatter = logging.Formatter('%(asctime)s [%(module)14s]'
                               '[%(levelname)8s] %(message)s')
@@ -26,10 +28,24 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 
 sys.excepthook = handle_exception
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--ss', action='store_true')
+parser.add_argument('--api', action='store_true')
+parser.add_argument('--ad', action='store_true')
+parser.add_argument('--date', type=str)
+parser.add_argument('--exp', choices=['all', 'db', 'ftp'])
+args = parser.parse_args()
+
 
 def main():
-    sc = sit.SiteConfig()
-    sc.take_screenshots()
+    sc = sit.SiteConfig(ss_file_path_date=args.date)
+    if args.ss or args.ad:
+        sc.take_screenshots()
+    if args.api:
+        sc.upload_screenshots()
+    if args.exp:
+        eh = exp.ExportHandler()
+        eh.export_loop(args.exp)
 
 
 if __name__ == '__main__':
